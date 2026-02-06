3e21fb3211a4dd6282870ae9207f0b3bf5025334
GIAN GABRIEL NARCISO DINIZ <gian.gndiniz@senacsp.edu.br>
2026-02-06 09:45:38 -0300

    .\.git\

--- Desafio_2.php
+++ Desafio_2.php
@@ -1,21 +1,13 @@
-<?php 
+<?php
 
+$valor1 = "10";
+$valor2 = "5";
 
+$numeros = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte'];
 
-$valor1 = "10"; 
-$valor2 = "5"; 
-
-$numeros = ['um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte' ];
-
-     
-if (is_numeric($valor1) && is_numeric($valor2))
-        {
-        $soma = $valor1 + $valor2;
-        echo "Resultado: " . $numeros[$valor1-1 ] . " mais " . $numeros[$valor2-1] . " é igual a " . $numeros[$soma-1];     
-        } 
-        else {
-        echo "Valores inválidos para a soma.";
-    }
-    
-
-?> 
+if (is_int($valor1) && is_int($valor2)) {
+    $soma = $valor1 + $valor2;
+    echo "Resultado: " . $numeros[$valor1 - 1] . " mais " . $numeros[$valor2 - 1] . " é igual a " . $numeros[$soma - 1];
+} else {
+    echo "Valores inválidos.";
+}
